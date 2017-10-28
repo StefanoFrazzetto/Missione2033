9130359e94fb36da45c747347c64dd4d77994722
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-10-28 15:18:38 +0100

    Add method to get only the Entity list

--- src/main/java/server/controllers/StatusController.java
+++ src/main/java/server/controllers/StatusController.java
@@ -1,10 +1,16 @@
 package server.controllers;
 
+import gameobjects.Entity;
 import gameobjects.GameEngine;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server.Application;
 import server.responses.Status;
+import utils.Serializer;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * StatusController
@@ -19,5 +25,27 @@ public class StatusController {
         GameEngine gameEngine = Application.getEngine();
 
         return Status.fromGameEngine(gameEngine);
+    }
+
+    @RequestMapping("/status/entities")
+    public EntityListResponse entities() throws IOException {
+        GameEngine gameEngine = Application.getEngine();
+
+        return new EntityListResponse(gameEngine.getEntityList());
+    }
+
+    /**
+     * Class to handle the response with only the Entity list.
+     */
+    public class EntityListResponse {
+        public String getSerializedEntityList() {
+            return serializedEntityList;
+        }
+
+        private final String serializedEntityList;
+
+        public EntityListResponse(List<Entity> entityList) throws IOException {
+            this.serializedEntityList = Serializer.toString((Serializable) entityList);
+        }
     }
 }
