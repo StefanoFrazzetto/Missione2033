df9bbb852ddd1107a4280dcad7784cb945f8e179
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-10-28 14:32:59 +0100

    Fixed position inheritance

--- src/main/java/gameobjects/Character.java
+++ src/main/java/gameobjects/Character.java
@@ -13,30 +13,13 @@ abstract class Character extends Entity implements Serializable {
 
     private int health;
 
-    private int xCoordinate;
 
-    private int yCoordinate;
 
     Character(int health) {
         this.health = health;
     }
 
-    public void setCoordinates(int x, int y) {
-        this.xCoordinate = x;
-        this.yCoordinate = y;
-    }
 
-    public int getxCoordinate() {
-        return xCoordinate;
-    }
-
-    public int getyCoordinate() {
-        return yCoordinate;
-    }
-
-    public Point getCoordinates() {
-        return new Point(xCoordinate, yCoordinate);
-    }
 
     /**
      * @return the entity health
--- src/main/java/gameobjects/Entity.java
+++ src/main/java/gameobjects/Entity.java
@@ -1,5 +1,7 @@
 package gameobjects;
 
+import java.awt.*;
+
 /**
  * Entity
  *
@@ -7,4 +9,24 @@ package gameobjects;
  * @version 1.0.0
  */
 public class Entity {
+    private int xCoordinate;
+
+    private int yCoordinate;
+
+    public void setCoordinates(int x, int y) {
+        this.xCoordinate = x;
+        this.yCoordinate = y;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public Point getCoordinates() {
+        return new Point(xCoordinate, yCoordinate);
+    }
 }
