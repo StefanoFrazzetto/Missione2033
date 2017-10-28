bfe4230e33a0d2c3d9ef37671334941eb17dcfcf
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-10-28 19:01:26 +0100

    Remove trash

--- src/main/java/gameobjects/Agent.java
+++ src/main/java/gameobjects/Agent.java
@@ -11,21 +11,11 @@ public class Agent extends Character {
     /** The default health value */
     private final static int DEFAULT_PLAYER_HEALTH = 100;
 
-//    /** The player type */
-//    private PlayerType playerType;
 
     /**
      * Create a new player.
      */
     public Agent() {
         super(DEFAULT_PLAYER_HEALTH);
-//        this.playerType = playerType;
     }
-
-//    /**
-//     * @return the playerType
-//     */
-//    public PlayerType getPlayerType() {
-//        return playerType;
-//    }
 }
