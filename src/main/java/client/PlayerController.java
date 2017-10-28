e4a04a2bd014e3528679a919a3324295eb59014f
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-10-28 17:53:14 +0100

    Add objects colors and letters for ports

--- src/main/java/client/PlayerController.java
+++ src/main/java/client/PlayerController.java
@@ -1,20 +1,28 @@
 package client;
 
-import gameobjects.Agent;
-import gameobjects.Entity;
-import gameobjects.GameGrid;
-import gameobjects.GameObject;
+import gameobjects.*;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public abstract class PlayerController {
+
+    public static final Color ENEMY_COLOR = Color.PURPLE;
+    public static final Color WALL_COLOR = Color.BLACK;
+    public static final Color FLOOR_COLOR = Color.WHITE;
+    public static final Color OPEN_DOOR_COLOR = Color.GREEN;
+    public static final Color CLOSED_DOOR_COLOR = Color.RED;
+    public static final Color EXIT_COLOR = Color.ORANGE;
+
     public static final int SIZE = 25;
     public GridPane gridPane;
 
@@ -58,9 +66,6 @@ public abstract class PlayerController {
                     case FLOOR:
                         rect.setFill(Color.WHITE);
                         break;
-                    case PLAYER:
-                        rect.setFill(Color.GREEN);
-                        break;
                 }
             }
 
@@ -69,23 +74,38 @@ public abstract class PlayerController {
         }
 
         if(showCharacters())
-        for (Entity asd : model.entityList) {
+        for (Entity entity : model.entityList) {
+            Node node = null;
             Rectangle rect = new Rectangle(SIZE, SIZE);
-
             rect.setFill(Color.RED);
 
-            if (asd instanceof Agent) {
+            if (entity instanceof Door) {
+                if (((Door) entity).isOpen()) {
+                    rect.setFill(OPEN_DOOR_COLOR);
+                } else {
+                    rect.setFill(CLOSED_DOOR_COLOR);
+                }
+
+                Text text = new Text(((Door) entity).getType().getStringSymbol());
+                StackPane stack = new StackPane();
+                stack.getChildren().add(rect);
+                stack.getChildren().add(text);
+                node = stack;
+
+            } else if (entity instanceof Enemy) {
+                rect.setFill(ENEMY_COLOR);
+            } else if (entity instanceof Agent) {
                 ImagePattern imagePattern = new ImagePattern(image);
-
-                //image.is
-
                 WritableImage wImage = new WritableImage(SIZE, SIZE);
-
-
                 rect.setFill(imagePattern);
+            } else if (entity instanceof Exit) {
+                rect.setFill(EXIT_COLOR);
             }
 
-            gridPane.add(rect, asd.getyCoordinate(), asd.getxCoordinate());
+            if (node == null) {
+                node = rect;
+            }
+            gridPane.add(node, entity.getyCoordinate(), entity.getxCoordinate());
         }
 
         gridPane.getScene().getWindow().sizeToScene();
