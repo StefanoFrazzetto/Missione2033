4fa45f8d97022ffdb5b88a407a6addbcba2f2539
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-10-28 15:45:04 +0100

    Create separate response class for the Entity list

---
+++ src/main/java/server/responses/EntityList.java
@@ -0,0 +1,27 @@
+package server.responses;
+
+import gameobjects.Entity;
+import utils.Serializer;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Class to handle the response with only the Entity list.
+ *
+ * @author stefano
+ * @version 1.0.0
+ */
+public class EntityList {
+
+    private final String serializedEntityList;
+
+    public EntityList(List<Entity> entityList) throws IOException {
+        this.serializedEntityList = Serializer.toString((Serializable) entityList);
+    }
+
+    public String getSerializedEntityList() {
+        return serializedEntityList;
+    }
+}
