1a79fbd4415a85f036bcfde55f2cb9a05981724b
Stefano Frazzetto <s.frazzetto22@gmail.com>
2017-10-28 11:14:57 +0100

    Add missing annotation

--- src/main/java/server/controllers/StatusController.java
+++ src/main/java/server/controllers/StatusController.java
@@ -2,6 +2,7 @@ package server.controllers;
 
 import gameobjects.GameEngine;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import server.Application;
 import server.responses.Status;
 
@@ -13,6 +14,7 @@ import java.io.IOException;
  * @author stefano
  * @version 1.0.0
  */
+@RestController
 public class StatusController {
     @RequestMapping("/status")
     public Status status() {
