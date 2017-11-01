698b63ba633f2895e4cd2ecc2409427e95592cd7
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-11-01 16:01:45 +0000

    Fixed PlayerModel.java

--- src/main/java/client/PlayerModel.java
+++ src/main/java/client/PlayerModel.java
@@ -4,19 +4,19 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import game.entities.Entity;
-import game.GameGrid;
+import game.Entities;
+import game.Grid;
+import game.Level;
+import game.gridobjects.GridObject;
 import utils.Serializer;
 
 import java.io.IOException;
-import java.util.List;
 
 public abstract class PlayerModel {
-    protected GameGrid gameGrid;
-    protected List<Entity> entityList;
+    protected Level level;
 
-    public GameGrid getGameGrid() {
-        return gameGrid;
+    public Grid<GridObject> getGameGrid() {
+        return level.getGameGrid();
     }
 
     public void updateGridGrid() throws UnirestException, IOException, ClassNotFoundException {
@@ -24,11 +24,11 @@ public abstract class PlayerModel {
                 .header("accept", "application/json")
                 .asJson();
 
-        String serializedGameGrid = jsonResponseStatus.getBody().getObject().getString("serializedGameGrid");
+        String serializedLevel = jsonResponseStatus.getBody().getObject().getString("serializedLevel");
 
-        gameGrid = (GameGrid) Serializer.fromString(serializedGameGrid);
+        level = (Level) Serializer.fromString(serializedLevel);
 
-        assert gameGrid != null;
+        assert level != null;
     }
 
     public void updateEntityList() throws UnirestException, IOException, ClassNotFoundException {
@@ -49,7 +49,19 @@ public abstract class PlayerModel {
         String serializedEntityList = jsonResponseStatus.getBody().getObject().getString("serializedEntityList");
 
         //noinspection unchecked
-        entityList = (List<Entity>) Serializer.fromString(serializedEntityList);
+        Entities entities = (Entities) Serializer.fromString(serializedEntityList);
+
+        level.getEntities().clear();
+        level.getEntities().addAll(entities);
+    }
+
+    protected void updateLevelFromJsonResponse(HttpResponse<JsonNode> jsonResponseStatus) throws UnirestException, IOException, ClassNotFoundException {
+        String serializedLevel = jsonResponseStatus.getBody().getObject().getString("serializedLevel");
+
+        //noinspection unchecked
+        level = (Level) Serializer.fromString(serializedLevel);
+
+        assert level != null;
     }
 
     public void initGame() throws UnirestException {
@@ -63,22 +75,11 @@ public abstract class PlayerModel {
 
         System.out.println(estimatedTime + " ms elapsed for the init request.");
 
-        String serializedGameGrid = jsonResponse.getBody().getObject().getString("serializedGameGrid");
-        String serializedEntityList = jsonResponse.getBody().getObject().getString("serializedEntityList");
-
         try {
-            gameGrid = (GameGrid) Serializer.fromString(serializedGameGrid);
-
-            assert gameGrid != null;
-
-            //noinspection unchecked
-            entityList = (List<Entity>) Serializer.fromString(serializedEntityList);
-
-            assert entityList != null;
-
-            // We need some heavier validation here
+            updateLevelFromJsonResponse(jsonResponse);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 }
